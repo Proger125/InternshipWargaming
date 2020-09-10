976ce60b4f7eb3a54de9db7a5c8a2a680bba12b4
Alexander Dyachenka <alexdyachenka@gmail.com>
2020-09-10 20:15:59 +0300

    TaskA

---
+++ TaskA/TaskA/TaskA.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n, k;
+  int counter = 0;
+  int factor = 1;
+  cin >> n >> k;
+  for (int i = 1; i <= n; i++) {
+    if (k % i == 0 && k / i <= n) {
+      counter++;
+    }
+  }
+  cout << counter;
+}
